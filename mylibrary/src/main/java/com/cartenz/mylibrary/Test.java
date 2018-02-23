67dfaa667dc473c08ed7b2a4584aea667a5c27ac
Pratama Budiman <pratamax@gmail.com>
2018-02-23 10:20:16 +0700

    push

---
+++ mylibrary/src/main/java/com/cartenz/mylibrary/Test.java
@@ -0,0 +1,14 @@
+package com.cartenz.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by pratama on 2/23/2018.
+ */
+
+public class Test {
+    public static void print() {
+        Log.wtf("Test_", "print");
+    }
+
+}
